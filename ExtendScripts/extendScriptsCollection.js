64a456dc9e8b27d888059514a3ef46cbe4a8a0c5
Subliminal_[var] <subliminal_kid@posteo.de>
2022-03-02 16:24:04 +0000

    Update ExtendScriptCollection.js

--- ExtendScripts/extendScriptsCollection.js
+++ ExtendScripts/extendScriptsCollection.js
@@ -4,7 +4,7 @@ function loadJson() {
 
 loadJson();
 
-// Ändere die Namen der Vorlagensequenzen mit dem Projektnamen
+// NEUES SCRIPT: Ändere die Namen der Vorlagensequenzen mit dem Projektnamen
 
 var anzahlSequenzen = app.project.sequences.numSequences
 
@@ -28,8 +28,8 @@ function changeSeqNames(projectName) {
 return seqName
 }
 
-
-// Mappt den Grafikpfad G:\\VPMS\\01-Grafik\\ auf \\\\Sp-isis01.ad.rbb-online.de\\data101$\\common\\088-01-Grafik\\, und G:\VPMS\14-MoJo auf \\\\sb-fbp-ist01.ad.rbb-online.de\\Transfer\\MoJo\\ wenn ein Asset in die Timeline gezogen wird
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// NEUES SCRIPT: Mappt den Grafikpfad G:\\VPMS\\01-Grafik\\ auf \\\\Sp-isis01.ad.rbb-online.de\\data101$\\common\\088-01-Grafik\\, und G:\VPMS\14-MoJo auf \\\\sb-fbp-ist01.ad.rbb-online.de\\Transfer\\MoJo\\ wenn ein Asset in die Timeline gezogen wird
 
 
 app.bind("onActiveSequenceTrackItemAdded", onTrackItemAdded)
@@ -50,5 +50,124 @@ function replaceMediaPath(trackItem) {
     }
 }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// NEUES SCRIPT: Das folgende Script gibt ein Array aus, mit allen Assets die nicht in den Transfer Ordnern Helmut, dem VPMS Share, im Grafik- oder Mojo-Ordner liegen
 
 
+
+var pathToCheck = "\\\\sb-fbp-ist01\\Transfer\\CNC\\Helmut\\Helmut4"
+var path2ToCheck = "\\\\sb-fbp-ist01\\VPMS\\Ingest\\IN_MaterialPool"
+var path3ToCheck = "\\\\sp-fbp-ist01\\VPMS\\Ingest\\IN_MaterialPool"
+var path4ToCheck = "\\\\Sp-isis01.ad.rbb-online.de\\data101$\\common\\088-01-Grafik"
+var path5ToCheck = "\\\\sb-fbp-ist01.ad.rbb-online.de\\Transfer\\MoJo"
+
+arrClipPaths = []
+arrClipNames = []
+CheckPathOfTimelineClips(app.project.activeSequence)
+
+JSON.stringify(CheckPathOfTimelineClips(app.project.activeSequence))
+
+function CheckPathOfTimelineClips(mysequence) {
+    if (mysequence) {
+        activeSequence = app.project.activeSequence
+        videoTracks = activeSequence.videoTracks
+        audioTracks = activeSequence.audioTracks
+        nameSeq = activeSequence.name
+        allSeq = app.project.sequences
+        var clipType = videoTracks
+        checkPath(clipType);            //checkt alle Clips auf den vorhandenen VIDEOtracks und schreibt fehlende Clips ins Array arrClipPaths
+
+        clipType = audioTracks;
+        checkPath(clipType)             //checkt alle Clips auf den vorhandenen AUDIOtracks und schreibt fehlende Clips ins Array arrClipPaths
+
+        ReturnClipPaths();              //gibt alle Elemente des Arrays arrClipPaths aus
+        return PathObjString
+    }
+
+
+    else {
+        return {"message":"Keine aktive Sequenz vorhanden"}
+    }
+}
+
+
+function checkPath(CLIP) {
+    for (var i = 0; i < CLIP.numTracks; i++) {
+        var track = CLIP[i]
+        var TrackClips = track.clips
+        //var Anzclips = TrackClips.numItems
+        for (var j = 0; j < TrackClips.numItems; j++) {
+            var TrackClip = TrackClips[j]
+            if (!TrackClip.disabled) {
+                var PrItem = TrackClip.projectItem
+                if (PrItem && PrItem.type == ProjectItemType.CLIP) {
+                    //$.writeln(PrItem.name)
+                    var clipPath = PrItem.getMediaPath()
+                    var clipName = PrItem.name
+                    var clipPathIndex = clipPath.indexOf(pathToCheck) // wenn der Pfad nicht dem pathToCheck enspricht wird -1 ausgegeben
+                    var clipPath2Index = clipPath.indexOf(path2ToCheck)
+                    var clipPath3Index = clipPath.indexOf(path3ToCheck)
+                    var clipPath4Index = clipPath.indexOf(path4ToCheck)
+                    var clipPath5Index = clipPath.indexOf(path5ToCheck)
+
+                    if (clipPathIndex == -1 && clipPath2Index == -1 && clipPath3Index == -1 && clipPath4Index == -1  && clipPath5Index == -1) { //überprüft ob der Clip Pfad bereits im Array vorhanden ist, falls nicht wird er ans Ende gepusht     
+                        for (var k = 0; k < arrClipPaths.length; k++) {
+                            var arrCHECK
+                            if (arrClipPaths[k] == clipPath) { arrCHECK = true; break }
+                            else (arrCHECK = false)
+                        }
+                        if (!arrCHECK) { 
+                            (arrClipPaths.push(clipPath));
+                            (arrClipNames.push(clipName))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+function ReturnClipPaths() {
+    var PathObj ={}
+    if (arrClipPaths.length > 0) {
+        
+        PathObj.Anzahl = arrClipPaths.length
+        PathObj.Pfade = arrClipPaths
+        PathObj.Names = arrClipNames
+        PathObj.lastPath = "0"
+    }
+        
+    else {
+        PathObj.Anzahl = 0 
+    }
+        PathObjString = JSON.stringify(PathObj)
+        PathObjString = PathObjString.replace(/\\\\/g,"/")
+        return PathObjString
+    }
+
+
+/*
+Wir suchen uns aus den im Projekt verwendeten Sequenzen die Sequenz heraus die wir exportiert haben. 
+Sobald wie diese kennen, fragen wir die frameSizeVertical und frameSizeHorizontal ab und schicken diese weiter an die nächste Node.
+*/
+
+
+
+var projektSequenzen = app.project.sequences
+
+var anzahlSequenzen = app.project.sequences.numSequences
+
+function FrameSize(nodeId){
+    var eachID
+    for (i=0; i < anzahlSequenzen; i++){
+        eachID = projektSequenzen[i].sequenceID
+        eachID = eachID.toString()
+        if (eachID === nodeId) {
+            var verticalSize = app.project.sequences[i].frameSizeVertical
+            verticalSize = verticalSize.toString()
+            var horizontalSize = app.project.sequences[i].frameSizeHorizontal
+            horizontalSize = horizontalSize.toString()
+            return horizontalSize + "X" + verticalSize
+        }
+    }   
+
+}
