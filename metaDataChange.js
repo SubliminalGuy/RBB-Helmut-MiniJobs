9ce86e94be16415fa0b3f5a5a8ee952dc64a34e6
Subliminal_[var] <subliminal_kid@posteo.de>
2022-04-04 13:55:31 +0000

    Add exportFindFrameSize.js

--- metaDataChange.js
+++
@@ -1,7 +0,0 @@
-var bla = require('./job.json')
-
-module.exports = function metaDataChange() {
-  var Material = bla.metadata.filter(item => item.name == "Sendedatum")
-  Material[0].value = 0
-  return Material[0]
-}
